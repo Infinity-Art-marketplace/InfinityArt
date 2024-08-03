425d98bdb490ff013d4bfcbb8aca33abe9314fbe
x-dev-private <gabrielcostaribeiro666@gmail.com>
2024-08-03 10:11:10 -0300

    add framework tailwind

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import HeaderApp from '../libs/headerApp';
+import HeaderApp from '../libs/headerApp'; // Verifique se o caminho está correto
+import '../App.css'; // Certifique-se de que o Tailwind CSS está configurado corretamente
 
 function Home() {
   return (
     <div>
       <HeaderApp />
+      <h1 className="text-4xl">Testando uma parada aqui</h1>
     </div>
   );
 }

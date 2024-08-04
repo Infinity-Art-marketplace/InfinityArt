cc67b6b413414fdba4007bf0b5b1d9eb41e21ce4
x-dev-private <gabrielcostaribeiro666@gmail.com>
2024-08-04 11:11:50 -0300

    front animation

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,19 +9,37 @@ function Home() {
   const firstSetOfNftCards = Array.from({ length: 4 }, (_, index) => <NftCard key={`first-${index}`} />);
   const secondSetOfNftCards = Array.from({ length: 4 }, (_, index) => <NftCard key={`second-${index}`} />);
 
-  const toggleNftCards = () => {
-    setIsFirstSet(!isFirstSet);
+  const showFirstSet = () => {
+    setIsFirstSet(true);
+  };
+
+  const showSecondSet = () => {
+    setIsFirstSet(false);
   };
 
   return (
-    <div className='flex flex-col h-screen'>
+    <div className='flex flex-col h-screen bg-gradient-to-b from-white to-gray-300'>
       <Header />
-      <div className='flex flex-wrap justify-center m-4'>
-        {isFirstSet ? firstSetOfNftCards : secondSetOfNftCards}
-      </div>
-      <button onClick={toggleNftCards} className='m-4 p-2 bg-blue-500 text-white rounded'>
-        Toggle NFT Cards
-      </button>
+      <section className='flex flex-col'>
+        <div className='flex justify-center items-center text-4xl'>
+          <a>collection name</a>
+        </div>
+        <div className='flex flex-wrap justify-center m-4'>
+          <button
+            onClick={showFirstSet}
+            className='m-4 p-2 text-4xl text-black rounded transition-transform duration-200 ease-in-out hover:text-gray-500 hover:scale-105'
+          >
+            &lt;
+          </button>
+          {isFirstSet ? firstSetOfNftCards : secondSetOfNftCards}
+          <button
+            onClick={showSecondSet}
+            className='m-4 p-2 text-4xl text-black rounded transition-transform duration-200 ease-in-out hover:text-gray-500 hover:scale-105'
+          >
+            &gt;
+          </button>
+        </div>
+      </section>
     </div>
   );
 }

364e498b4c11103124a96b9f4aab9f1971fb9d30
x-dev-private <gabrielcostaribeiro666@gmail.com>
2024-08-03 23:45:48 -0300

    animation in nft card

--- src/components/nftCard.js
+++ src/components/nftCard.js
@@ -3,8 +3,10 @@ import NFTimage from './cropped_image_512x512.png';
 
 const NftCard = () => {
     return (
-        <div className="max-w-sm rounded shadow-lg m-4">
-            <img className="w-232 h-232 object-cover" src={NFTimage} alt="NFTImage" />
+        <div className="max-w-sm rounded shadow-lg m-4 transition-transform hover:scale-105">
+            <div className="flex justify-center items-center h-232">
+                <img className="w-232 object-cover" src={NFTimage} alt="NFT Image" />
+            </div>
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">connective autonomy</div>
                 <p className="text-gray-700 text-base">

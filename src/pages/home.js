49838ed3515dd0d78b707cdb6ff5c15c26df334b
x-dev-private <gabrielcostaribeiro666@gmail.com>
2024-08-04 00:04:06 -0300

    toggle button

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../libs/header';
 import '../App.css';
 import NftCard from '../components/nftCard';
 
 function Home() {
-  const nftCards = Array.from({ length: 4 }, (_, index) => <NftCard key={index} />);
+  const [isFirstSet, setIsFirstSet] = useState(true);
+
+  const firstSetOfNftCards = Array.from({ length: 4 }, (_, index) => <NftCard key={`first-${index}`} />);
+  const secondSetOfNftCards = Array.from({ length: 4 }, (_, index) => <NftCard key={`second-${index}`} />);
+
+  const toggleNftCards = () => {
+    setIsFirstSet(!isFirstSet);
+  };
 
   return (
     <div className='flex flex-col h-screen'>
       <Header />
       <div className='flex flex-wrap justify-center m-4'>
-        {nftCards}
+        {isFirstSet ? firstSetOfNftCards : secondSetOfNftCards}
       </div>
+      <button onClick={toggleNftCards} className='m-4 p-2 bg-blue-500 text-white rounded'>
+        Toggle NFT Cards
+      </button>
     </div>
   );
 }

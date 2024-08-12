173c35ea4013ad1c4718516a61916d24ebc1a8fa
x-dev-private <gabrielcostaribeiro666@gmail.com>
2024-08-12 08:55:09 -0300

    a

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "infinityart": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "storage": {
+    "dynamo9bf5c6b9": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
---
+++ src/graphql/queries.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getUsers = /* GraphQL */ `
+  query GetUsers {
+    getUsers {
+      account
+      username
+      image
+      __typename
+    }
+  }
+`;
+export const getNFTs = /* GraphQL */ `
+  query GetNFTs {
+    getNFTs {
+      contract
+      image
+      name
+      collection
+      value
+      __typename
+    }
+  }
+`;
